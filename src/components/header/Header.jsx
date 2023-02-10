d48503bf15827c83abc22a15f92ac2321a98ebe8
dankturtle <abrowncownow@gmail.com>
2023-02-09 20:57:42 -0800

    scroll down adjusted

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,13 +13,15 @@ const Header = () => {
         <h5 className="text-light">Full-Stack Developer</h5>
         <CTA />
 
+        <HeaderSocials />
+
         <div className="me">
           <img src={ME} alt="me" />
         </div>
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
 
-        <HeaderSocials />
+        
       </div>
     </header>
   )
