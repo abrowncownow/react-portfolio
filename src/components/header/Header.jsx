b22e85a9cf04af669ed3d90656ad89c696235085
dankturtle <abrowncownow@gmail.com>
2023-02-09 20:36:35 -0800

    header and header socials added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/meNChase.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Alex Brown</h1>
+        <h5 className="text-light">Full-Stack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+        <HeaderSocials />
+      </div>
+    </header>
   )
 }
 
